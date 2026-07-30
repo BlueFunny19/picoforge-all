317c2bb15075b9c7f8204975e7320acc6473ae46
Suyog Tandel <git@suyogtandel.in>
2026-07-30 22:57:37 +0530

    chore: bump app ver to 0.9 and update metainfo

--- src/ui/screens/about/view.rs
+++ src/ui/screens/about/view.rs
@@ -36,7 +36,7 @@ impl Render for AboutViewModel {
                                             .text_color(theme.foreground)
                                             .child("PicoForge"),
                                     )
-                                    .child(Tag::new("v0.8.0"))
+                                    .child(Tag::new("v0.9.0"))
                                     .child(
                                         div()
                                             .text_color(theme.muted_foreground)
